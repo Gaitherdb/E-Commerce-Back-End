6ae6d524e4050ec4c55a543740362014d7c04a5c
David Gaither <gaitherdb@gmail.com>
2021-08-24 17:29:30 -0400

    edit ProductTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,30 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id',
+      },
+    }
   },
   {
     sequelize,
